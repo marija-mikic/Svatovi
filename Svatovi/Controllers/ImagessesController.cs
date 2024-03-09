e1760f93c63579573b1232d48093d01441c00ee6
marija-mikic <93914251+marija-mikic@users.noreply.github.com>
2024-03-09 12:30:49 +0100

    ading controller add new book

--- Svatovi/Controllers/ImagessesController.cs
+++ Svatovi/Controllers/ImagessesController.cs
@@ -48,6 +48,26 @@ namespace Svatovi.Controllers
             return View(model);
         }
 
+        private async Task<IActionResult> AddNewimage(ImagessModel imagemodel)
+        {
+
+            if (imagemodel.Gallerys! == null)
+            {
+                string folder = "image/gallery/";
+                imagemodel.Gallerys = new List<GalleryModel>();
+                foreach(var file in imagemodel.Gallerys) {
+                    var gallery = new GalleryModel()
+                    {
+                        Name = file.Name,
+                        URL = file.URL
+                    };
+                    imagemodel.Gallerys.Add(gallery);
+
+                }
+            }
+            return View();
+        }
+
         private async Task<string>UploadImage(string folderPath, IFormFile file)
         {
             folderPath += Guid.NewGuid().ToString() + "_" + file.FileName;
