e76c1b596a50739ee25e3fd63c6215ae3b642ceb
marija-mikic <93914251+marija-mikic@users.noreply.github.com>
2024-04-07 00:14:43 +0200

    finish image

--- Svatovi/wwwroot/js/site.js
+++ Svatovi/wwwroot/js/site.js
@@ -2,20 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-var slideIndex = 1;
-showDivs(slideIndex);
+var slideIndex = 0;
 
-function plusDivs(n) {
-    showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
+function showSlides() {
     var i;
-    var x = document.getElementsByClassName("slide");
-    if (n > x.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = x.length }
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
+    var slides = document.getElementsByClassName("slide-div");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-    x[slideIndex - 1].style.display = "block";
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
