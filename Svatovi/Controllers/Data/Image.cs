fdbb41286c861788a6b7f69d8900e42d7ed19777
marija-mikic <93914251+marija-mikic@users.noreply.github.com>
2024-04-26 00:20:14 +0200

    no changes

---
+++ Svatovi/Controllers/Data/Image.cs
@@ -0,0 +1,17 @@
+﻿namespace Svatovi.Data
+{
+    public class Image
+    {
+
+        public int Id {  get; set; }
+        //public string Images { get; set; }
+
+        public string Coment { get; set; }
+
+        public ICollection<ImageGallery> imageGalleries { get; set; }   
+
+
+
+
+    }
+}
